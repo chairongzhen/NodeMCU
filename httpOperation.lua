74b3cd4f77049b4d7deed1a4fbf4c1aaccc8ed3c
ERIC <241498527@qq.com>
2017-07-07 12:50:10 +0800

    Add files via upload

--- httpOperation.lua
+++ httpOperation.lua
@@ -2,33 +2,27 @@
 dofile('httpServer.lua')
 httpServer:listen(80)
 dofile("LightOpr.lua")
--- Custom API
--- Light the Chip Light
+
+
+
+-- Operation of the Light
 httpServer:use('/buttonLight', function(req, res)
          gpio.write(buttonLigh, gpio.LOW)
          gpio.write(chipLight, gpio.HIGH)
+         res:redirect('index.html?callback=button')
 end)
 
 httpServer:use('/chipLight', function(req, res)     
          gpio.write(buttonLigh, gpio.HIGH)
          gpio.write(chipLight, gpio.LOW)
-end)
-
-
--- Light the Button Light
-httpServer:use('/buttonlighton', function(req, res)
-        gpio.write(buttonLigh, gpio.HIGH)
-        gpio.write(chipLight, gpio.LOW)
-end)
-
--- Get json
-httpServer:use('/json', function(req, res)
-    res:type('application/json')
-    res:send('{"doge": "smile"}')
+         res:redirect('index.html?callback=chip')
 end)
 
 -- Close All Light
 httpServer:use('/closeall', function(req, res)
     gpio.write(chipLight, gpio.HIGH)
     gpio.write(buttonLigh, gpio.HIGH)
+    res:redirect('index.html?callback=closeall')
 end)
+
+
